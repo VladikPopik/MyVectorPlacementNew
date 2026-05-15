da8c91c665066d29f3788e8581a3beadb3c98897
VladikPopik <vlunku@gmail.com>
2026-05-15 15:09:26 +0300

    Vector EmplaceBack

--- vector.h
+++ vector.h
@@ -12,34 +12,29 @@ public:
   RawMemory() = default;
 
   explicit RawMemory(size_t capacity)
-      : buffer_(static_cast<T *>(::operator new(capacity * sizeof(T)))),
+      : buffer_(capacity == 0
+                    ? nullptr
+                    : static_cast<T *>(::operator new(capacity * sizeof(T)))),
         capacity_(capacity) {}
 
   ~RawMemory() { ::operator delete(buffer_); }
 
-  RawMemory(RawMemory &&other) noexcept
-      : buffer_(other.buffer_), capacity_(other.capacity_) {
-    other.buffer_ = nullptr;
-    other.capacity_ = 0;
-  }
+  RawMemory(const RawMemory &) = delete;
+  RawMemory &operator=(const RawMemory &) = delete;
+
+  RawMemory(RawMemory &&other) noexcept { Swap(other); }
 
   RawMemory &operator=(RawMemory &&other) noexcept {
     if (this != &other) {
-      ::operator delete(buffer_);
-      buffer_ = other.buffer_;
-      capacity_ = other.capacity_;
-      other.buffer_ = nullptr;
-      other.capacity_ = 0;
+      RawMemory tmp(std::move(other));
+      Swap(tmp);
     }
     return *this;
   }
 
-  RawMemory(const RawMemory &) = delete;
-  RawMemory &operator=(const RawMemory &) = delete;
-
-  T *data() { return buffer_; }
-  const T *data() const { return buffer_; }
-  size_t capacity() const { return capacity_; }
+  constexpr T *data() noexcept { return buffer_; }
+  constexpr const T *data() const noexcept { return buffer_; }
+  constexpr size_t capacity() const noexcept { return capacity_; }
 
   void Swap(RawMemory &other) noexcept {
     std::swap(buffer_, other.buffer_);
@@ -52,6 +47,19 @@ private:
 };
 
 template <typename T> class Vector {
+private:
+  static constexpr bool MoveIsNothrow = std::is_nothrow_move_constructible_v<T>;
+  static constexpr bool CanCopy = std::is_copy_constructible_v<T>;
+  static constexpr bool ShouldMoveSafely = MoveIsNothrow || !CanCopy;
+
+  void RelocateElements(T *dst) {
+    if constexpr (ShouldMoveSafely) {
+      std::uninitialized_move_n(data_.data(), size_, dst);
+    } else {
+      std::uninitialized_copy_n(data_.data(), size_, dst);
+    }
+  }
+
 public:
   Vector() = default;
 
@@ -63,40 +71,31 @@ public:
     std::uninitialized_copy_n(other.data_.data(), other.size_, data_.data());
   }
 
-  Vector(Vector &&other) noexcept
-      : data_(std::move(other.data_)), size_(other.size_) {
-    other.size_ = 0;
+  Vector(Vector &&other) noexcept : size_(std::exchange(other.size_, 0)) {
+    data_.Swap(other.data_);
   }
 
   ~Vector() { std::destroy_n(data_.data(), size_); }
 
   Vector &operator=(const Vector &rhs) {
-    if (this != &rhs) {
-      if (data_.capacity() >= rhs.size_) {
-        const size_t common = std::min(size_, rhs.size_);
-        for (size_t i = 0; i < common; ++i) {
-          data_.data()[i] = rhs.data_.data()[i];
-        }
-        if (size_ < rhs.size_) {
-          size_t i = size_;
-          try {
-            for (; i < rhs.size_; ++i) {
-              new (data_.data() + i) T(rhs.data_.data()[i]);
-            }
-          } catch (...) {
-            std::destroy_n(data_.data() + size_, i - size_);
-            throw;
-          }
-        }
-        if (size_ > rhs.size_) {
-          std::destroy_n(data_.data() + rhs.size_, size_ - rhs.size_);
-        }
-        size_ = rhs.size_;
-      } else {
-        Vector temp(rhs);
-        Swap(temp);
-      }
+    if (this == &rhs)
+      return *this;
+
+    if (data_.capacity() < rhs.size_) {
+      Vector temp(rhs);
+      Swap(temp);
+      return *this;
     }
+
+    if (size_ >= rhs.size_) {
+      std::copy(rhs.data_.data(), rhs.data_.data() + rhs.size_, data_.data());
+      std::destroy_n(data_.data() + rhs.size_, size_ - rhs.size_);
+    } else {
+      std::copy(rhs.data_.data(), rhs.data_.data() + size_, data_.data());
+      std::uninitialized_copy_n(rhs.data_.data() + size_, rhs.size_ - size_,
+                                data_.data() + size_);
+    }
+    size_ = rhs.size_;
     return *this;
   }
 
@@ -110,130 +109,86 @@ public:
     std::swap(size_, other.size_);
   }
 
-  size_t Size() const { return size_; }
-  size_t Capacity() const { return data_.capacity(); }
+  constexpr size_t Size() const noexcept { return size_; }
+  constexpr size_t Capacity() const noexcept { return data_.capacity(); }
 
-  T &operator[](size_t index) { return data_.data()[index]; }
-  const T &operator[](size_t index) const { return data_.data()[index]; }
+  T &operator[](size_t index) noexcept { return data_.data()[index]; }
+  const T &operator[](size_t index) const noexcept {
+    return data_.data()[index];
+  }
+
+  template <typename... Args> T &EmplaceBack(Args &&...args) {
+    if (size_ < data_.capacity()) {
+      T *item_ptr = ::new (static_cast<void *>(data_.data() + size_))
+          T(std::forward<Args>(args)...);
+      ++size_;
+      return *item_ptr;
+    }
+
+    const size_t new_capacity = size_ == 0 ? 1 : size_ * 2;
+    RawMemory<T> new_data(new_capacity);
+
+    T *item_ptr = ::new (static_cast<void *>(new_data.data() + size_))
+        T(std::forward<Args>(args)...);
+
+    try {
+      RelocateElements(new_data.data());
+    } catch (...) {
+      std::destroy_at(item_ptr);
+      throw;
+    }
+
+    std::destroy_n(data_.data(), size_);
+    data_ = std::move(new_data);
+    ++size_;
+    return *item_ptr;
+  }
 
   void Reserve(size_t new_capacity) {
-    if (new_capacity > data_.capacity()) {
-      RawMemory<T> new_data(new_capacity);
-      if constexpr (std::is_nothrow_move_constructible_v<T>) {
-        std::uninitialized_move_n(data_.data(), size_, new_data.data());
-      } else if constexpr (std::is_copy_constructible_v<T>) {
-        std::uninitialized_copy_n(data_.data(), size_, new_data.data());
-      } else {
-        std::uninitialized_move_n(data_.data(), size_, new_data.data());
-      }
-      std::destroy_n(data_.data(), size_);
-      data_ = std::move(new_data);
-    }
+    if (new_capacity <= data_.capacity())
+      return;
+
+    RawMemory<T> new_data(new_capacity);
+    RelocateElements(new_data.data());
+
+    std::destroy_n(data_.data(), size_);
+    data_ = std::move(new_data);
   }
 
   void Resize(size_t new_size) {
     if (new_size < size_) {
       std::destroy_n(data_.data() + new_size, size_ - new_size);
       size_ = new_size;
-    } else if (new_size > size_) {
-      if (new_size > data_.capacity()) {
-        const size_t new_capacity = std::max(new_size, data_.capacity() * 2);
-        RawMemory<T> new_data(new_capacity);
-
-        if constexpr (std::is_nothrow_move_constructible_v<T>) {
-          std::uninitialized_move_n(data_.data(), size_, new_data.data());
-        } else if constexpr (std::is_copy_constructible_v<T>) {
-          std::uninitialized_copy_n(data_.data(), size_, new_data.data());
-        } else {
-          std::uninitialized_move_n(data_.data(), size_, new_data.data());
-        }
-
-        size_t i = size_;
-        try {
-          for (; i < new_size; ++i) {
-            new (new_data.data() + i) T();
-          }
-        } catch (...) {
-          std::destroy_n(new_data.data() + size_, i - size_);
-          std::destroy_n(new_data.data(), size_);
-          throw;
-        }
-
-        std::destroy_n(data_.data(), size_);
-        data_ = std::move(new_data);
-        size_ = new_size;
-      } else {
-        size_t i = size_;
-        try {
-          for (; i < new_size; ++i) {
-            new (data_.data() + i) T();
-          }
-        } catch (...) {
-          std::destroy_n(data_.data() + size_, i - size_);
-          throw;
-        }
-        size_ = new_size;
-      }
+      return;
     }
+
+    if (new_size <= data_.capacity()) {
+      std::uninitialized_value_construct_n(data_.data() + size_,
+                                           new_size - size_);
+      size_ = new_size;
+      return;
+    }
+
+    const size_t new_capacity = std::max(new_size, data_.capacity() * 2);
+    RawMemory<T> new_data(new_capacity);
+
+    std::uninitialized_value_construct_n(new_data.data() + size_,
+                                         new_size - size_);
+
+    try {
+      RelocateElements(new_data.data());
+    } catch (...) {
+      std::destroy_n(new_data.data() + size_, new_size - size_);
+      throw;
+    }
+
+    std::destroy_n(data_.data(), size_);
+    data_ = std::move(new_data);
+    size_ = new_size;
   }
 
-  void PushBack(const T &value) {
-    if (size_ == data_.capacity()) {
-      const size_t new_capacity = size_ == 0 ? 1 : size_ * 2;
-      RawMemory<T> new_data(new_capacity);
-
-      if constexpr (std::is_nothrow_move_constructible_v<T>) {
-        std::uninitialized_move_n(data_.data(), size_, new_data.data());
-      } else if constexpr (std::is_copy_constructible_v<T>) {
-        std::uninitialized_copy_n(data_.data(), size_, new_data.data());
-      } else {
-        std::uninitialized_move_n(data_.data(), size_, new_data.data());
-      }
-
-      try {
-        new (new_data.data() + size_) T(value);
-      } catch (...) {
-        std::destroy_n(new_data.data(), size_);
-        throw;
-      }
-
-      std::destroy_n(data_.data(), size_);
-      data_ = std::move(new_data);
-      ++size_;
-    } else {
-      new (data_.data() + size_) T(value);
-      ++size_;
-    }
-  }
-
-  void PushBack(T &&value) {
-    if (size_ == data_.capacity()) {
-      const size_t new_capacity = size_ == 0 ? 1 : size_ * 2;
-      RawMemory<T> new_data(new_capacity);
-
-      if constexpr (std::is_nothrow_move_constructible_v<T>) {
-        std::uninitialized_move_n(data_.data(), size_, new_data.data());
-      } else if constexpr (std::is_copy_constructible_v<T>) {
-        std::uninitialized_copy_n(data_.data(), size_, new_data.data());
-      } else {
-        std::uninitialized_move_n(data_.data(), size_, new_data.data());
-      }
-
-      try {
-        new (new_data.data() + size_) T(std::move(value));
-      } catch (...) {
-        std::destroy_n(new_data.data(), size_);
-        throw;
-      }
-
-      std::destroy_n(data_.data(), size_);
-      data_ = std::move(new_data);
-      ++size_;
-    } else {
-      new (data_.data() + size_) T(std::move(value));
-      ++size_;
-    }
-  }
+  void PushBack(const T &value) { EmplaceBack(value); }
+  void PushBack(T &&value) { EmplaceBack(std::move(value)); }
 
   void PopBack() noexcept {
     --size_;
